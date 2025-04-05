c9964da76592e460e7c7c7ce5561a147603f6f9f
qwerty7z <qwerty@gmail.com>
2025-04-05 15:32:36 +0500

    add my portfolio

--- js/data.js
+++ js/data.js
@@ -1,8 +1,17 @@
 export default [
   {
+    title: "Uzbekistan",
+    github: "https://github.com/qwerty7z/uzbekistan",
+    vercel: "https://uzbekistan-seven.vercel.app/",
+  },
+  {
+    title: "3-dars",
+    github: "https://github.com/qwerty7z/3-dars",
+    vercel: "https://coffe-mu-one.vercel.app/",
+  },
+  {
     title: "Akadem Nashr",
     github: "https://github.com/qwerty7z/akadem-nashr",
     vercel: "https://akadem-nashr-rose.vercel.app/",
   },
-
 ];

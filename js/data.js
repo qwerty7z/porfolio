e78d6c5ba66c9539d65ba008b5fe052ca1bee768
qwerty7z <qwerty@gmail.com>
2025-04-16 15:09:50 +0500

    add

--- js/data.js
+++ js/data.js
@@ -1,17 +1,42 @@
 export default [
   {
-    title: "Uzbekistan",
-    github: "https://github.com/qwerty7z/uzbekistan",
-    vercel: "https://uzbekistan-seven.vercel.app/",
+    title: "Book",
+    github: "https://github.com/qwerty7z/home",
+    vercel: "https://book-club-lime.vercel.app/",
   },
+
   {
-    title: "3-dars",
-    github: "https://github.com/qwerty7z/3-dars",
-    vercel: "https://coffe-mu-one.vercel.app/",
+    title: "Chanel Parfum",
+    github: "https://github.com/qwerty7z/home",
+    vercel: "https://home-psi-sand.vercel.app/",
+  },
+
+  {
+    title: "Reklama",
+    github: "https://github.com/qwerty7z/12-dars",
+    vercel: "https://1-card.vercel.app/",
+  },
+
+  {
+    title: "Sport",
+    github: "https://github.com/qwerty7z/12-dars",
+    vercel: "https://sport-ruddy.vercel.app/",
+  },
+
+  {
+    title: "New YORK",
+    github: "https://github.com/qwerty7z/new-york",
+    vercel: "https://new-york-st37.vercel.app/",
   },
   {
     title: "Akadem Nashr",
     github: "https://github.com/qwerty7z/akadem-nashr",
     vercel: "https://akadem-nashr-rose.vercel.app/",
+  },
+
+  {
+    title: "Uzbekistan",
+    github: "https://github.com/qwerty7z/uzbekistan",
+    vercel: "https://uzbekistan-seven.vercel.app/",
   },
 ];
